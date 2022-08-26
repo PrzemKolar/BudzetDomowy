ed133f4fe06bc2cc18cd4c8a6cb266552f349f71
PrzemKolar <97679012+PrzemKolar@users.noreply.github.com>
2022-08-26 22:56:00 +0200

    Szkielet warstwy DataBase

---
+++ BudzetDomowy/BudzetDomowy.DataAccess/HouseholdBudgetStorageContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudzetDomowy.DataAccess
+{
+    public class HouseholdBudgetStorageContext : DbContext
+    {
+        public HouseholdBudgetStorageContext(DbContextOptions<HouseholdBudgetStorageContext> opt) : base(opt)
+        {
+
+        }
+
+    }
+}
---
+++ BudzetDomowy/BudzetDomowy.DataAccess/HouseholdBudgetStorageContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetDomowy.DataAccess
+{
+    public class HouseholdBudgetStorageContextFactory : IDesignTimeDbContextFactory<HouseholdBudgetStorageContext>
+    {
+        public HouseholdBudgetStorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<HouseholdBudgetStorageContext>();
+            optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=HouseholdBudgetStorage;Integrated Security=True");
+            return new HouseholdBudgetStorageContext(optionsBuilder.Options);
+        }
+    }
+}
