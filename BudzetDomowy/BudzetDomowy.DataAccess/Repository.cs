4758a48ca1b9deffd7f7d06a911ea9455e6e3d7c
PrzemKolar <97679012+PrzemKolar@users.noreply.github.com>
2022-08-31 23:32:37 +0200

    Dodanie generycznego repozytorium DJ

---
+++ BudzetDomowy/BudzetDomowy.DataAccess/Repository.cs
@@ -0,0 +1,60 @@
+﻿using BudzetDomowy.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetDomowy.DataAccess
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        protected readonly HouseholdBudgetStorageContext context;
+        private DbSet<T> entities;
+
+        public Repository(HouseholdBudgetStorageContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return entities.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(T entity)
+        {
+            if(entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            T entity = entities.SingleOrDefault(x => x.Id == id);
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
